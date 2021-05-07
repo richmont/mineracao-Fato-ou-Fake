0f82421f921c6b9578ddac0a2c491d3097602ad9
richelmy <richelmy.monteiro@gmail.com>
2021-05-07 18:34:09 -0300

    Tokens ignoram numerais e verbos, consulta sql de palavras frequentes salvo

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from conf.settings import SQLITE_POSTS_FILENAME, SQLITE_TOKENS_FILENAME
 import time
 
 start_time = time.time()
+
 #print("ainda não fiz literalmente nada além de importar ", time.time() - start_time, "seconds")
 cursor_posts = conectar(SQLITE_POSTS_FILENAME)
 cursor_tokens = conectar(SQLITE_TOKENS_FILENAME)
@@ -14,7 +15,7 @@ tTokens.criar_tabela()
 lista_posts = tPosts.consulta_post_titulo("É #FAKE que", limite=200000)
 #print("Tempo de consulta ao banco ", time.time() - start_time, "seconds")
 # é preciso adicionar números na lista de stopwords
-stopwords_customizadas = {'É', 'FATO', "FAKE", "mostre", 'após', 'logo', 'R'}
+stopwords_customizadas = {'É', 'FATO', "FAKE", "mostre", 'após', 'logo', 'R', 'ser', 'dizer', 'usar'}
 #stopwords_customizadas = None
 lista_tokens = []
 # carrega o idioma português no spacy apenas uma vez, não mais a cada token criado
@@ -29,5 +30,9 @@ for x in lista_posts:
     #print("Tokens lematizados", token.tokens_lematizados)
     #print("Título original: ", token.texto)
     tTokens.inserir_token(token._id, token.tokens_lematizados)
-    
+
+
+#cursor_tokens = conectar(SQLITE_TOKENS_FILENAME)
+#tTokens = TabelaTokens(cursor_tokens)
+
 print(time.time() - start_time, "seconds")
--- preprocess.py
+++ preprocess.py
@@ -106,8 +106,10 @@ class Token:
                 # excecão para palavras com hífen adicionada
                 # resultados de dois engines de stopwords divergem
                 # como o melhor resultado são menos stopwords, usaremos ambos
-                if x.text.isalnum() and (x.text not in self.lista_stopwords) or ("-" in x.text) and (not x.is_stop):
+                
+                if x.text.isalnum() and (x.pos_ != 'VERB' and x.pos_ != 'NUM') and (x.text not in self.lista_stopwords) or ("-" in x.text) and (not x.is_stop):
                     #print("após o if ", time.time() - start_time, "seconds")
+                    print("a palavra " + x.text + " é um " + x.pos_ + " e foi inserida ao banco")
                     self.tokens_filtrados.append(x)
                     #if x.pos_ == 'PROPN':
                     #    self.nomes_proprios.append(x.text)
